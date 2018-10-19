5478247db1c6dfb4529311a2849f80c5f6944ff8
scrollbar-ww <scrollbar@163.com>
2018-10-19 11:08:03 +0800

    support "mixc(color, alpha)"

--- postcss-mix-color.esm.js
+++ postcss-mix-color.esm.js
@@ -3,12 +3,19 @@ import balanced from 'balanced-match'
 import Color from 'color'
 import messageHelpers from 'postcss-message-helpers'
 
-const mix = (c1, c2, w = '') => {
-  const weight = w.endsWith('%') ? w.replace('%', '') : w * 100
-  return Color(c1).mix(Color(c2), weight / 100).string()
+function mix (c1, c2, w) {
+  if (arguments.length === 1) {
+    return c1
+  } else if (arguments.length === 2 && (c2.endsWith('%') || parseFloat(c2) <= 1)) {
+    w = c2
+    c2 = undefined
+  }
+  if (!w) w = '50%'
+  const weight = (w.endsWith('%') ? w.replace('%', '') : w * 100) / 100
+  return c2 ? Color(c1).mix(Color(c2), weight).string() : Color(c1).alpha(w).string()
 }
 
-const transformValue = (string, source) => {
+function transformValue (string, source) {
   const idx = string.indexOf('mixc(')
   if (idx < 0) return string
   if (idx >= 0) {
@@ -21,7 +28,7 @@ const transformValue = (string, source) => {
   }
 }
 
-const transformDecl = decl => {
+function transformDecl (decl) {
   if (decl.value && decl.value.indexOf('mixc(') > -1) {
     messageHelpers.try(
       () => {
